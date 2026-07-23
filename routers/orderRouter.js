199305e3f5121a07113813fc19b7fe54a0c52715
Thejan2006 <hmagthejanbandar@gmail.com>
2026-07-23 23:18:21 +0530

    add routes

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { createOrder, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
+import { 
+    createOrder, 
+    getAllOrders, 
+    updateOrderStatus 
+} from "../controllers/orderController.js";
 
-const orderRouter = express.Router()
+import authenticate from "../middlewares/authenticate.js"; 
 
-orderRouter.post("/" , createOrder)
-orderRouter.get("/:pageNumber/:pageSize" , getAllOrders)
-orderRouter.put("/:orderId" , updateOrderStatus)
-export default orderRouter
+const router = express.Router();
+
+
+router.post("/", authenticate, createOrder);
+
+
+router.get("/:pageNumber/:pageSize", authenticate, getAllOrders);
+
+
+router.put("/:orderId", authenticate, updateOrderStatus);
+
+export default router;
