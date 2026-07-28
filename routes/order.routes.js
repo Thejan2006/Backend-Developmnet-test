148dc6c79a6cea660a7bf81ffac3a7111708cb11
Thejan2006 <hmagthejanbandar@gmail.com>
2026-07-28 17:57:08 +0530

    remove some comment

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -8,13 +8,13 @@ import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router()
 
-// 1. Order එකක් Place කිරීම (User Protected)
+
 router.post("/", authMiddleware, createOrder);
 
-// 2. සියලුම Orders ලබාගැනීම (User / Admin Protected)
+
 router.get("/:pageNumber/:pageSize", authMiddleware, getAllOrders);
 
-// 3. Order Status එක Update කිරීම (Admin Protected)
+
 router.put("/:orderId", authMiddleware, updateOrderStatus);
 
 export default router;
