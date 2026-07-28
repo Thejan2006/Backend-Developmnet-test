cc76216cd3538e624b9df1b231a7735d6117e04f
Thejan2006 <hmagthejanbandar@gmail.com>
2026-07-28 17:51:42 +0530

    import authmiddleware

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -4,11 +4,17 @@ import {
     getAllOrders, 
     updateOrderStatus 
 } from "../controllers/orderController.js"
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router()
 
-router.post("/", createOrder)
-router.get("/:pageNumber/:pageSize", getAllOrders)
-router.put("/:orderId", updateOrderStatus)
+// 1. Order එකක් Place කිරීම (User Protected)
+router.post("/", authMiddleware, createOrder);
 
-export default router
+// 2. සියලුම Orders ලබාගැනීම (User / Admin Protected)
+router.get("/:pageNumber/:pageSize", authMiddleware, getAllOrders);
+
+// 3. Order Status එක Update කිරීම (Admin Protected)
+router.put("/:orderId", authMiddleware, updateOrderStatus);
+
+export default router;
